5e87a5f6af735fc5913e25d0d64895a1fe7dfb50
Murat Demirci <mdemirci01@gmail.com>
2021-06-08 11:32:12 +0300

    build deleted

--- build/Exceptions/conusma-exception.d.ts
+++
@@ -1,7 +0,0 @@
-export declare class ConusmaException extends Error {
-    constructor(_type: string, _message: string, _ex?: Error);
-    detailEx: Error;
-    message: string;
-    type: string;
-    className: string;
-}
