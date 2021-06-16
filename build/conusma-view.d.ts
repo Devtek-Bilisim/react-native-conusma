673ceb3559a76227d49b800eb5ee8c46243342f0
hdoru <huseyindoru01@gmail.com>
2021-06-16 14:28:47 +0300

    close consuemr fix

--- build/conusma-view.d.ts
+++ build/conusma-view.d.ts
@@ -1,4 +1,3 @@
 import { Component } from 'react';
 export declare class ConusmaView extends Component {
-    render(): boolean;
 }
--- build/conusma-view.js
+++ build/conusma-view.js
@@ -12,17 +12,6 @@ var __extends = (this && this.__extends) || (function () {
         d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
     };
 })();
-var __assign = (this && this.__assign) || function () {
-    __assign = Object.assign || function(t) {
-        for (var s, i = 1, n = arguments.length; i < n; i++) {
-            s = arguments[i];
-            for (var p in s) if (Object.prototype.hasOwnProperty.call(s, p))
-                t[p] = s[p];
-        }
-        return t;
-    };
-    return __assign.apply(this, arguments);
-};
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.ConusmaView = void 0;
 var react_1 = require("react");
@@ -31,9 +20,6 @@ var ConusmaView = /** @class */ (function (_super) {
     function ConusmaView() {
         return _super !== null && _super.apply(this, arguments) || this;
     }
-    ConusmaView.prototype.render = function () {
-        return (__assign({}, this.props) > /RTCView>););
-    };
     return ConusmaView;
 }(react_1.Component));
 exports.ConusmaView = ConusmaView;
